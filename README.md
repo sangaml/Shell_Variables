136b31c42e408e432bc9781e230d01c423b7ded1
sangaml <sangamlonk@gmail.com>
2018-02-16 11:33:57 -0800

    Input valuse then code this value as variable

---
+++ README.md
@@ -0,0 +1,1 @@
+# Shell Variables, Take INPUT from user
---
+++ variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+echo -n "Enter your name :"
+read name         #Take Input from User and use as variable in the script
+echo "Hi "$name", It's fine day isn'i it?"  
